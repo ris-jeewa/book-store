0f91d50104a631824eaf4e2fbcede7d302837821
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-11-07 10:20:24 +0530

    feat: create api to get all users

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UsersModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      }
+    }
+  );
+  await app.listen();
 }
 bootstrap();
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  private users: UserDto[] = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane kane' },
+  ]
+
+  findAll() {
+    return this.users;
   }
 }
